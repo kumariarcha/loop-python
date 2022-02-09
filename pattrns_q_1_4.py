4f786ab78c782696623cf56152e838beaa182f9a
Archana singh <95541918+kumariarcha@users.noreply.github.com>
2022-02-09 10:44:06 +0530

    Add files via upload

---
+++ pattrns_q_1_4.py
@@ -0,0 +1,27 @@
+# i=1
+# while i<5:
+#     j=1
+#     while j<5:
+#         print(j,end=" ")
+#         j=j+1
+#     print( )
+#     i=i+1
+
+
+i=1
+while i<5:
+    j=1
+    while j<5:
+        print(i,end=" ")
+        j=j+1
+    print( )
+    i=i+1
+
+# i=4
+# while i>0:
+#     j=4
+#     while j>0:
+#         print(i,end=" ")
+#         j=j-1
+#     print()
+#     i=i-1
