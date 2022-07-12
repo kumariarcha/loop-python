70d9c72d77865817a20f7c0ec2a6699766c2c87b
Archana singh <95541918+kumariarcha@users.noreply.github.com>
2022-07-12 16:31:40 +0530

    Add files via upload

---
+++ reverse_num.py
@@ -0,0 +1,35 @@
+# num=int(input("enter the num"))
+# rev=0
+# while num>0:  
+#     rev=(rev*10)+num%10
+#     num=num//10
+#     print("num=",rev)
+
+# a=int(input("enter the num:-"))
+# rev=0
+# while a>0   :
+#     rev=(rev*10)+a%10
+#     a=a//10
+#     print("a=",rev)
+
+# m=int(input("enter the num:-"))
+# rev=0 
+# while m>0:
+#     rev=(rev*10)+m%10
+#     m=m//10
+#     print("m=",rev)
+
+# p=int(input("enter the num:-"))
+# m=0
+# while p>0:
+#     m=(m*10)+p%10
+#     p=p//10
+#     print("p=",m)
+
+
+# p=int(input("enter the num:-"))
+# q=0
+# while p>0:
+#     q=(q*10)+p%10
+#     p=p//10
+#     print("p=",q)
