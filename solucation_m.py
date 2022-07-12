bcd4012846646df6ab748ec1bac76a85253f0f82
Archana singh <95541918+kumariarcha@users.noreply.github.com>
2022-07-12 16:31:09 +0530

    Add files via upload

---
+++ solucation_m.py
@@ -0,0 +1,96 @@
+# i=1
+# while i<=7:
+#     print("#"*i)
+#     i=i+1
+
+
+
+# i=1
+# while i<=100:
+#     if i%3==0 and i%7==0:
+#         print("navgurukul")
+#     elif i%3==0:
+#         print("nav")
+#     elif i%7==0:
+#         print("gurukul")
+#     else:
+#         print(i)
+#     i=i+1
+
+
+# i=1
+# while i<=20:
+#     print(i)
+#     i=i+1
+
+# i=1
+# while i<=100:
+#     print(i)
+#     i=i+1
+
+# i=1
+# while i<=200:
+#     print(i)
+#     i=i+1
+
+# i=200
+# while i>0:
+#     print(i)
+#     i=i-1
+
+
+
+# i=500
+# while i<=1000:
+#     print(i)
+#     i=i+1
+
+
+# a="archana"
+# b="78"
+# c="devika"
+# print(a+c+b)
+
+
+# name="archana"
+# name2="devika"
+# name3="hema"
+# num=9
+# num1=str(num)
+# print(name+name2+name3+num1)
+
+
+
+# x=5
+# y="john"
+# print(type(x))
+# print(type(y))
+
+
+# a=2**3*5*1
+# print(a)
+
+
+# a=4*5**4*2
+# print(a)
+
+
+# a=5*4**4*3
+# print(a)
+
+# c=2*3**4*5
+# print(c)
+
+
+# a=2*2+4%2+6**2+6+(2+6)
+# print(a)
+# op-54
+
+
+# a=5+7-9//8+6%2+(3//6/2-6)
+# print(a)
+
+# a=6<7 or 7<6
+# print(a)
+
+ 
