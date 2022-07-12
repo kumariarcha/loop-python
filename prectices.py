28f0e91134b72531bc20687eed3f4cf19ae9ebbb
Archana singh <95541918+kumariarcha@users.noreply.github.com>
2022-07-12 16:49:14 +0530

    Add files via upload

---
+++ prectices.py
@@ -0,0 +1,43 @@
+
+# num=int(input("enter the num"))
+# count=0
+# i=2
+# while i<=num:
+#     if num%i==0:
+#         count=count+2
+#     i=i+1
+# if count==2:
+#     print(num,"is prime")
+# else:
+#     print(num,"is not prime")
+
+
+# num=int(input("enter the num:-"))
+# sum=0
+# i=1
+# while i<=num:
+#     num2=int(input("enter the num:-"))
+#     sum=sum+num
+#     i=i+1
+# avrage=sum/num
+# print(sum,"this is sum num")
+# print(avrage,"this is avrage num")
+
+# guess=int(input("enter the num:-"))
+# chance=6
+# i=1
+# while i<=6:
+#     num=int(input("enter the num:-"))
+#     if num==guess:
+#         print("you have won")
+#         break
+#     else:
+#         print("you have chance")
+#     i=i+1
+#     chance=chance-1
+#     print(chance)    
+
+
+
+    
+
