0dc02727f53c7c3bbfe44233d6c472028728c046
Archana singh <95541918+kumariarcha@users.noreply.github.com>
2022-07-15 15:52:34 +0530

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,90 @@
+# num=int(input("enter the num:-"))
+# while num>0:
+#     print(num%2,end=" ")
+#     num=num//2
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         break
+#     print(i)
+#     i=i+1
+
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         break
+#     i=i+1
+#     print(i)
+
+
+
+# i=1
+# while i<=10:
+#     i=i+1
+#     if i==4:
+#         # i=i+1
+#         continue
+#     print(i)
+
+
+
+# i=1
+# while i<=5:
+#     print(i*i)
+#     i=i+1
+
+
+# user=int(input("enter the num:-"))
+# i=1
+# while i<=user:
+#     if i%10==0:
+#         print(i)
+#     i=i+1
+
+
+
+# i=105
+# while i>=1:
+#     if i%7==0:
+#         print(i)
+#     i=i-1
+
+
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i=i-1
+
+
+# i=1
+# sum=0
+# while i<=10:
+#     if i%2==0:
+#         sum=sum+i
+#         print(sum)
+#     i=i+1
+
+
+# i=2
+# i=i+2
+# while i<=20:
+#     i=i+1
+#     print(i)
+
+
+
+# n=int(input("enter the num:-"))
+# m=int(input("enter the num:-"))
+# i=2
+# while i<=m:
+#         j=1
+#         while j<=10:
+#                 print(str(i)+"X"+str(j)+"="+str(i*j))
+#                 j=j+1
+#         print()
+#         i=i+1
+
+
